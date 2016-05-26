b075b98fd2b0be20ea13ed6daf9446a35ae15ace
alpsank <alpsank@yahoo.com>
2016-05-25 21:58:13 -0700

    trying out

--- formoid_files/formoid1/form.php
+++ formoid_files/formoid1/form.php
@@ -1,9 +1,9 @@
 <?php
 
-define('EMAIL_FOR_REPORTS', '');
-define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
-define('FINISH_URI', 'http://');
-define('FINISH_ACTION', 'message');
+define('EMAIL_FOR_REPORTS', 'alpsank@gmail.com');
+define('RECAPTCHA_PRIVATE_KEY', '6Ldm8yATAAAAAFgBpoftqDqK2VYbgd4_6YXTwFBa');
+define('FINISH_URI', 'http://samarthandtishyame/thanks.html');
+define('FINISH_ACTION', 'redirect');
 define('FINISH_MESSAGE', 'Thanks for filling out my form!');
 define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
 
@@ -19,12 +19,15 @@ require_once _DIR_ . '/handler.php';
 <!-- Start Formoid form-->
 <link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-blue.css" type="text/css" />
 <script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
-<form class="formoid-solid-blue" style="background-color:#353432;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:680px;min-width:150px" method="post"><div class="title"><h2>My form</h2></div>
-	<div class="element-name<?php frmd_add_class("name1"); ?>" title="Enter your name"><label class="title"><span class="required">*</span></label><span class="nameFirst"><input placeholder="Name First" type="text" size="8" name="name1[first]" required="required"/><span class="icon-place"></span></span><span class="nameLast"><input placeholder="Name Last" type="text" size="14" name="name1[last]" required="required"/><span class="icon-place"></span></span></div>
-	<div class="element-email<?php frmd_add_class("email"); ?>" title="Enter your email address"><label class="title"></label><div class="item-cont"><input class="large" type="email" name="email" value="" placeholder="Your email"/><span class="icon-place"></span></div></div>
-	<div class="element-input<?php frmd_add_class("input1"); ?>" title="Message goes here"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input1" required="required" placeholder="Enter your wishes here"/><span class="icon-place"></span></div></div>
-	<div class="element-radio<?php frmd_add_class("radio"); ?>"><label class="title">I am coming!</label>		<div class="column column1"><label><input type="radio" name="radio" value="Yes" /><span>Yes</span></label><label><input type="radio" name="radio" value="No" /><span>No</span></label></div><span class="clearfix"></span>
-</div>
+<form class="formoid-solid-blue" style="background-color:#353432;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:680px;min-width:150px" method="post"><div class="title"><h2>Your wish goes here</h2></div>
+	<div class="element-input<?php frmd_add_class("input"); ?>" title="Enter your name here"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input" required="required" placeholder="Your Name"/><span class="icon-place"></span></div></div>
+	<div class="element-email<?php frmd_add_class("email"); ?>" title="Enter your name here"><label class="title"></label><div class="item-cont"><input class="large" type="email" name="email" value="" placeholder="Your email"/><span class="icon-place"></span></div></div>
+	<div class="element-textarea<?php frmd_add_class("textarea"); ?>" title="Enter message"><label class="title"><span class="required">*</span></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" required="required" placeholder="Message goes here"></textarea><span class="icon-place"></span></div></div>
+	<div class="element-recaptcha<?php frmd_add_class("captcha"); ?>" title="Are you a bot?"><label class="title"></label><script type="text/javascript">var RecaptchaOptions = {theme : "clean"};</script>
+<script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k=6Ldm8yATAAAAAIsRQ3yW8wb69XMLq9HdTX7fNTx5&theme=clean"></script>
+<noscript><iframe src="http://www.google.com/recaptcha/api/noscript?k=6Ldm8yATAAAAAIsRQ3yW8wb69XMLq9HdTX7fNTx5&hl=en" height="300" width="500" frameborder="0"></iframe></br>
+<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea><input type="hidden" name="recaptcha_response_field" value="manual_challenge"></noscript>
+<script type="text/javascript">if (/#invalidcaptcha$/.test(window.location)) (document.getElementById("recaptcha_widget_div")).className += " error"</script></div>
 <div class="submit"><input type="submit" value="Submit"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-solid-blue.js"></script>
 
 <!-- Stop Formoid form-->
